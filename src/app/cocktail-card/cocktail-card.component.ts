e06e1d1519f9fb537c736795f2a03039bb678587
Ramon Ramon <ramonsramon24@gmail.com>
2023-11-23 13:23:56 -0600

    Optimize images with NgOptimizedImage

--- src/app/cocktail-card/cocktail-card.component.ts
+++ src/app/cocktail-card/cocktail-card.component.ts
@@ -2,16 +2,18 @@ import { Component, Input, OnInit } from "@angular/core"
 import { Cocktail, EMPTY_COCKTAIL } from "../models/cocktail"
 import { BubbleComponent } from "../bubble/bubble.component"
 import { RouterLink } from "@angular/router"
+import { NgOptimizedImage } from "@angular/common"
 
 @Component({
     selector: "cocktail-card",
     standalone: true,
-    imports: [BubbleComponent, RouterLink],
+    imports: [BubbleComponent, RouterLink, NgOptimizedImage],
     templateUrl: "./cocktail-card.component.html",
     styleUrls: ["./cocktail-card.component.css"],
 })
 export class CocktailCardComponent implements OnInit {
     @Input({ required: true }) cocktail: Cocktail = EMPTY_COCKTAIL
+    @Input({ required: true }) cocktailIndex: number = 0
     cocktailLink: string = ""
     cocktailImage: string = ""
 
