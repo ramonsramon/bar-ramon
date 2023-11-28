716be05991127fc300a5bbfaffc1cbe8d2bd7fc4
Ramon Ramon <ramonsramon24@gmail.com>
2023-11-27 20:51:54 -0600

    Add indicators for new subscriber form submission

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,18 +7,29 @@ import {
 } from "@angular/forms"
 import { ContactForm } from "../models/contact"
 import { BarRamonService } from "../bar-ramon.service"
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner"
 import { MatIconModule } from "@angular/material/icon"
 import { RouterLink } from "@angular/router"
+import { Observable, catchError, of } from "rxjs"
+import { HttpResponse } from "@angular/common/http"
 
 @Component({
     selector: "footer",
     standalone: true,
-    imports: [MatIconModule, ReactiveFormsModule, RouterLink],
+    imports: [
+        MatIconModule,
+        ReactiveFormsModule,
+        RouterLink,
+        MatProgressSpinnerModule,
+    ],
     templateUrl: "./footer.component.html",
     styleUrl: "./footer.component.css",
 })
 export class FooterComponent {
     newSubForm: FormGroup<ContactForm>
+    isLoading: boolean = false
+    isSuccess: boolean = false
+    isFailure: boolean = false
 
     constructor(private barRamonService: BarRamonService) {
         this.newSubForm = new FormGroup({
@@ -27,13 +38,23 @@ export class FooterComponent {
     }
 
     submit() {
+        this.isLoading = !this.isLoading
         this.barRamonService
             .addSubscriber(this.newSubForm.getRawValue())
+            .pipe(
+                catchError((err) => {
+                    return of(err) as Observable<HttpResponse<string>>
+                })
+            )
             .subscribe((res) => {
-                if (res.status === 200) {
-                    this.newSubForm.reset()
+                this.isLoading = !this.isLoading
+                if (res.status === 200 || res.status === 409) {
+                    this.isSuccess = true
+                    this.isFailure = false
                 } else {
                     console.log(res)
+                    this.isSuccess = false
+                    this.isFailure = true
                 }
             })
     }
