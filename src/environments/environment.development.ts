e3fc909662b66e94d00fec24c139a29e9b80c9d2
Ramon Ramon <ramonsramon24@gmail.com>
2024-05-18 13:28:04 -0500

    Check in environment files
    Originally I did not check in the environment files due to the fact that there is an API Key
    present in them. However, Firebase docs directly reference that it is okay to public display
    the API Key as they do not control access to backend resources https://firebase.google.com/docs/projects/api-keys

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyCLSK9LYb2hB0qIKkhIT3vdpbv1N7WOsMo",
+        authDomain: "bar-ramon.firebaseapp.com",
+        projectId: "bar-ramon",
+        storageBucket: "bar-ramon.appspot.com",
+        messagingSenderId: "1086487753405",
+        appId: "1:1086487753405:web:fd047134cf574cfa41bdd4",
+        measurementId: "G-DWJBXE84Y1",
+    },
+    databaseId: "test",
+    emailServiceUrl: "https://email.aggieramon.com",
+}
